e2a9b9d83f396df03a254b67e637e2bdc98e36da
Branimirius <73712742+Branimirius@users.noreply.github.com>
2023-01-25 23:19:35 +0100

    django backend init & uns app bug fix

--- uns/index.js
+++ uns/index.js
@@ -22,10 +22,18 @@ app.post('/student', (req, res) => {
       return res.status(500).send(err);
     }
     if (result.rows.length === 0) {
-      return res.status(404).send({ message: 'Student not found.' });
+      // If student does not exist, insert student into the database
+      client.query('INSERT INTO students(name, email) VALUES($1, $2)', [name, email], (err, result) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        return res.status(201).send({ message: 'Student added.' });
+      });
+    } else {
+      return res.status(200).send(result.rows[0]);
     }
-    return res.status(200).send(result.rows[0]);
   });
 });
+
 
 app.listen(3000, () => console.log('Server started on port 3000'));
